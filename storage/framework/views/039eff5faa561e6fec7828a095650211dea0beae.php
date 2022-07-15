447f9e9e527b8614092f35a614d396c60b088f22
NurAisyaHeri <84298280+NurAisyaHeri@users.noreply.github.com>
2022-07-15 18:22:43 +0800

    Add files via upload

---
+++ storage/framework/views/039eff5faa561e6fec7828a095650211dea0beae.php
@@ -0,0 +1,104 @@
+
+
+<?php $__env->startSection('content'); ?>
+    <div class="card mb-4">
+        <div class="card-header">
+            <?php echo e(__('Create New User')); ?>
+
+            <a class="btn btn-primary float-end" href="<?php echo e(route('users.index')); ?>"> Back</a>
+        </div>
+
+        <div class="card-body">
+
+            <?php if(count($errors) > 0): ?>
+                <div class="alert alert-danger">
+                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                    <ul>
+                        <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <li><?php echo e($error); ?></li>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
+
+                <?php echo Form::open(array('route' => 'users.store','method'=>'POST')); ?>
+
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Name:</strong>
+                            <?php echo Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')); ?>
+
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Email:</strong>
+                            <?php echo Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control')); ?>
+
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Password:</strong>
+                            <?php echo Form::password('password', array('placeholder' => 'Password','class' => 'form-control')); ?>
+
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Confirm Password:</strong>
+                            <?php echo Form::password('confirm-password', array('placeholder' => 'Confirm Password','class' => 'form-control')); ?>
+
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Role:</strong>
+                            <select class="form-control select2" name="roles[]" multiple>
+                                <?php $__currentLoopData = $roles; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $role): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    <option value="<?php echo e($role); ?>"> <?php echo e($role); ?> </option>
+                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Bio:</strong>
+                            <textarea class="form-control" name="biodata" class="bio" id="" cols="30" rows="10"></textarea>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12 text-center mt-2">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+
+                </div>
+
+                <?php echo Form::close(); ?>
+
+
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('script'); ?>
+    <script>
+        $(document).ready(function () {
+            let role = $('.select2');
+            role.select2({
+                placeholder: 'Select...',
+                width: '100%',
+                allowClear: true,
+                multiple: true
+            });
+        });
+    </script>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\lenovo\manajemen-user\manajemen-user\resources\views/users/create.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/04ffea0a07daef9707448275765e43e4da53f596.php
@@ -0,0 +1,16 @@
+
+
+<?php $__env->startSection('content'); ?>
+    <div class="card mb-4">
+        <div class="card-header">
+            <?php echo e(__('Dashboard')); ?>
+
+        </div>
+        <div class="card-body">
+            <?php echo e(__('You are logged In!')); ?>
+
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\lenovo\manajemen-user\manajemen-user\resources\views/home.blade.php ENDPATH**/ ?>
